dfcefc6857f0e4a3c1330d26f14af47b48e89fe3
zhouqinghai <2892910202@qq.com>
2017-08-17 15:35:19 +0800

    init

---
+++ upload/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost.com/opencart/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost.com/opencart/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost.com/opencart/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost.com/opencart/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/code/web/opencart/upload/admin/');
+define('DIR_SYSTEM', 'E:/code/web/opencart/upload/system/');
+define('DIR_IMAGE', 'E:/code/web/opencart/upload/image/');
+define('DIR_STORAGE', 'E:/code/web/opencart/storage/');
+define('DIR_CATALOG', 'E:/code/web/opencart/upload/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'zqh');
+define('DB_PASSWORD', 'tesT1234');
+define('DB_DATABASE', 'vj2');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
